ca5524bc3f3841e993267740fe1575b57c10979c
MaksIgnatchenko <foecunditate@gmail.com>
2018-03-30 02:09:25 +0300

    first step

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+//THIS FILE CONTAINS FRONT CONTROLLER
+session_start();
+define('ROOT', dirname(__FILE__));
+
+//Function for automatic connection of classes
+require_once(ROOT.'/components/Autoload.php');
+
+$router = new Router();
+$router -> run();
+
+
